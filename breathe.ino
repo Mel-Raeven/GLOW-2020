22ecfae6d1f4f5901ae4908ef02dadae495d9858
Mel Raeven <melraeven@MacBook-Pro-van-Mel.local>
2020-11-24 11:03:50 +0100

    removed stepper motor and added new LED's

--- breathe.ino
+++ breathe.ino
@@ -1,72 +1,78 @@
 #include <Adafruit_NeoPixel.h>
-#define dirPin 2
-#define stepPin 3
-#define LED_PIN 6
-#define LED_COUNT 30
-#define stepsPerRevolution 370
-int breathSpeed = 2000;
+#define LED_PIN 7
+#define LED_PIN2 6
+#define LED_COUNT 100
+#define stepsPerRevolution 255
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGBW);
+Adafruit_NeoPixel strip2(LED_COUNT, LED_PIN2, NEO_RGBW);
 
 void setup() {
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
   strip.begin();
   strip.show();
   Serial.begin(9600);
 }
 
 void loop() {
-  int breathControl = 0;
-  int breathOfset = 2;
+  int ledColor = 22000; // 22000 = red
+  int ledOpacity = 0;
   int ledBrightness = 0;
-//  int breathSpeed = 2000;
-  digitalWrite(dirPin, HIGH);
-//
-//  int number = rand() % 100;
-//  if (number > 50) {
-//    breathSpeed += 100;
-//  } else {
-//    breathSpeed -= 100;
-//  };
-//  if (breathSpeed > 3000) {
-//    breathSpeed == 2000;
-//  };
+  int breathSpeed = 4; // 0 = fast; 5 = slow
+  int breathPause = 2000; // 1000 = fast; 2000 = slow
 
-  //rotation forward
+  // RNG breath alternator
+  int number = rand() % 100;
+  if (number > 40) {
+    breathSpeed += 1;
+  } else {
+    breathSpeed -= 1;
+  };
+  
+  // breathSpeed catch
+  if (breathSpeed > 10) {
+    breathSpeed -= 1;
+  };
+
+  // breathSpeed color change
+  if (breathSpeed > 4) {
+    ledOpacity = 255;
+    ledColor = 22000;
+  }
+  if (breathSpeed < 4) {
+     ledOpacity = 0;
+     ledColor = 22000;
+  }
+
+  //breathe in
   for(int i = 0; i < stepsPerRevolution; i++)
     {
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(breathSpeed + breathControl);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(breathSpeed + breathControl);
-      breathControl += breathOfset;
       if (ledBrightness < 255) {
-        uint32_t rgbcolor = strip.ColorHSV(22000, 255, ledBrightness);
+        uint32_t rgbcolor = strip.ColorHSV(ledColor, ledOpacity, ledBrightness);
+        uint32_t rgbcolor2 = strip2.ColorHSV(ledColor, ledOpacity, ledBrightness);
         ledBrightness += 1;
         strip.fill(rgbcolor);
+        strip2.fill(rgbcolor2);
         strip.show();
+        strip2.show();
+        delay(breathSpeed);
       };
       Serial.println(ledBrightness);
     }
-  delay(2000);
-  breathControl = 0;
-  digitalWrite(dirPin, LOW);
+  delay(breathPause);
 
   ledBrightness = 255;
-  //rotation back
+  //breathe out
   for(int i = 0; i < stepsPerRevolution; i++)
     {
-      digitalWrite(stepPin, HIGH);
-      delayMicroseconds(breathSpeed + breathControl);
-      digitalWrite(stepPin, LOW);
-      delayMicroseconds(breathSpeed + breathControl);
-      breathControl += breathOfset;
       if (ledBrightness > 0) {
-        uint32_t rgbcolor = strip.ColorHSV(22000, 255, ledBrightness);
+        uint32_t rgbcolor = strip.ColorHSV(ledColor, ledOpacity, ledBrightness);
+        uint32_t rgbcolor2 = strip2.ColorHSV(ledColor, ledOpacity, ledBrightness);
         ledBrightness -= 1;
         strip.fill(rgbcolor);
+        strip2.fill(rgbcolor2);
         strip.show();
+        strip2.show();
+        delay(breathSpeed);
       };
     }
-    delay(2000);
+    delay(breathPause);
 }
